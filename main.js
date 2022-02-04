06ba057ef092f9ab765e776ad6782156e067b896
Chavi29 <98895162+Chavi29@users.noreply.github.com>
2022-02-05 01:24:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var num = prompt("PLEASE ENTER A NUMBER","");
+
+for (var i = 1 ; i <= 10; i++) {
+  document.write("<div>")
+  document.write(num + " X " + i + " = " + i*num)
+  
+    document.write("</div>");
+    
+
+  
+};
